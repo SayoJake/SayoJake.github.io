e8c42d824bcafd319b312a475f5de79747d72ef2
SayoJake <150616445+SayoJake@users.noreply.github.com>
2024-09-24 22:07:50 -0400

    Correctly updated

--- UI-Project-1/vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-
-export default defineConfig({
-  plugins: [svelte()],
-  base: '/UI-Project-1/', // Adjust the base path if needed
-});
